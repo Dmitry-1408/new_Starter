7d123f6ac7fe466579b9a3800f34468aacc3f976
Dmitry Bykov <bykovdmnic@outlook.com>
2024-11-29 11:52:51 +0300

    добавил задачи gulp
    компиляция html
    
    автообновление браузера

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -4,7 +4,9 @@ import gulpSass from 'gulp-sass'; // Импортируем gulp-sass
 import cleanCSS from 'gulp-clean-css';
 import autoprefixer from 'gulp-autoprefixer';
 import rename from 'gulp-rename';
-import { series } from 'gulp';
+import htmlmin from 'gulp-htmlmin';
+import browserSync from 'browser-sync'; // Для автообновления браузера
+import { series, parallel, watch } from 'gulp'; // Для создания комплексных задач
 
 // Инициализируем gulp-sass с компилятором sass
 const sassCompiler = gulpSass(sass);
@@ -12,13 +14,39 @@ const sassCompiler = gulpSass(sass);
 // Задача для компиляции SASS
 export const styles = () => {
   return gulp
-    .src('src/sass/style.sass') // Путь к вашему SASS-файлу
+    .src('src/sass/**/*.+(sass|scss)') // Путь к вашим SASS/SCSS-файлам
     .pipe(sassCompiler().on('error', sassCompiler.logError)) // Компиляция SASS
     .pipe(autoprefixer()) // Добавление вендорных префиксов
     .pipe(cleanCSS({ compatibility: 'ie8' })) // Минификация CSS
     .pipe(rename({ suffix: '.min' })) // Переименуем в style.min.css
-    .pipe(gulp.dest('dist/css')); // Сохраним в папку dist/css
+    .pipe(gulp.dest('dist/css')) // Сохраним в папку dist/css
+    .pipe(browserSync.stream()); // Обновляем браузер
 };
 
-// Задача по умолчанию
-export default series(styles);
+// Задача для минификации HTML
+export const html = () => {
+  return gulp
+    .src('src/html/**/*.html') // Путь к вашим HTML-файлам
+    .pipe(htmlmin({ collapseWhitespace: true, removeComments: true })) // Минификация
+    .pipe(gulp.dest('dist')) // Сохраняем в dist
+    .pipe(browserSync.stream()); // Обновляем браузер
+};
+
+// Задача для слежения за файлами и выполнения соответствующих задач
+export const watchFiles = () => {
+  watch('src/sass/**/*.+(sass|scss)', styles); // Наблюдение за стилями
+  watch('src/html/**/*.html', html); // Наблюдение за HTML
+  watch('dist/**/*').on('change', browserSync.reload); // Перезагрузка браузера при изменениях в dist
+};
+
+// Задача для запуска локального сервера и автообновления браузера
+export const server = () => {
+  browserSync.init({
+    server: {
+      baseDir: 'dist', // Указываем корневую папку
+    },
+  });
+};
+
+// Задача по умолчанию: запускаем все задачи одной командой
+export default series(parallel(styles, html), parallel(server, watchFiles));
